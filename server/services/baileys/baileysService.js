280ae80d16194977c46ebc574b3766dd53afbe05
hidra <yhidraxz@gmail.com>
2025-04-29 00:46:41 -0300

    created baileys first connection and basic store and auth state

---
+++ server/services/baileys/baileysService.js
@@ -0,0 +1,32 @@
+import makeWASocket, {
+  useMultiFileAuthState,
+  makeInMemoryStore,
+} from "baileys";
+
+const { state, saveCreds } = useMultiFileAuthState("baileys_auth_info");
+
+const store = makeInMemoryStore({});
+
+store.readFromFile("./baileys_store.json");
+
+setInterval(() => {
+  store.writeToFile("./baileys_store.json");
+}, 10000);
+
+const sock = makeWASocket({
+  printQRInTerminal: true,
+  auth: state,
+  syncFullHistory: false,
+});
+
+sock.ev.on("creds.update", saveCreds);
+
+store.bind(sock.ev);
+
+sock.ev.on("chats.upsert", () => {
+  console.log("got chats", store.chats.all());
+});
+
+sock.ev.on("contacts.upsert", () => {
+  console.log("got contacts", Object.values(store.contacts));
+});
