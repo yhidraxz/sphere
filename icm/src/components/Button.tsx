c80bd0d45e2ffbc131adaccbb514e0abe68d1669
joaninha do hidra <anacplsantos@gmail.com>
2025-05-07 01:08:10 -0300

    better css for buttons and form

--- icm/src/components/Button.tsx
+++ icm/src/components/Button.tsx
@@ -14,7 +14,7 @@ export const PrimaryButton: React.FC<buttonProps> = ({
   return (
     <div className="relative w-64 h-12 rounded-md overflow-hidden group ">
       {/* Gradient Background Layer (visible behind) */}
-      <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-700 z-1" />
+      <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-700 z-0" />
 
       {/* Solid Overlay Layer (fades out on hover) */}
       <div className="absolute inset-0 bg-lightBlue z-10 transition-opacity duration-500 ease-in-out group-hover:opacity-0" />
@@ -22,7 +22,7 @@ export const PrimaryButton: React.FC<buttonProps> = ({
       {/* Input Field */}
       <button
         type={type}
-        className={`${customCss}relative z-20 w-full h-full px-4 py-2 text-white bg-transparent outline-none placeholder-white`}
+        className={`${customCss} relative z-20 w-full h-full px-4 py-2 text-white bg-transparent outline-none placeholder-white`}
         onClick={onClick}
       >
         {text}
--- icm/src/components/secondaryButton.tsx
+++ icm/src/components/secondaryButton.tsx
@@ -14,7 +14,7 @@ export const SecondaryButton: React.FC<buttonProps> = ({
   return (
     <button
       type={type}
-      className={`${customCss}p-3 rounded-md text-white cursor-pointer text-base font-bold text-center 
+      className={`${customCss} p-3 rounded-md text-white cursor-pointer text-base font-bold text-center 
  bg-lightBlue rounded-[10px] outline-none border-none transition-all duration-200 
  ease-in-out delay-0 transform hover:bg-navy hover:scale-80`}
       onClick={onClick}
